b015a19461bcf9f2afc5b3b13df8fc210359a368
fujitsukaharuto <146906715+fujitsukaharuto@users.noreply.github.com>
2026-02-09 15:09:27 +0900

    変更

--- Texture/convert.ps1
+++ Texture/convert.ps1
@@ -1,8 +1,10 @@
+
+
 $files = Get-ChildItem *.jpg, *.png
 foreach ($f in $files) {
      $args = @(
         $f,  # ファイルパス
-        "-ml", "0"    # オプションをここに追加
+        "-ml", "3"    # オプションをここに追加
     )
     Start-Process -FilePath TexureConverter.exe -ArgumentList $args -Wait
 }
